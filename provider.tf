7ced52a483707103553c8cba655869a118dbb49b
irinadaniel <daniel-alexandru.irina@student.tuiasi.ro>
2026-04-04 10:40:53 +0000

    3

--- provider.tf
+++ provider.tf
@@ -17,7 +17,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "kdsjhfhdsohgiodshg6548"
+    bucket  = "asdasdasdedfgh"
     key     = "terraform.tfstate"
     region  = "eu-north-1"
     encrypt = true
